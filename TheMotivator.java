b41a6967dc41d45ab560b5a8b6bfc84e5f7bdd8b
s932344 <s932344@gmail.com>
2017-02-28 03:14:53 -0600

    Add feedback for 80

--- TheMotivator.java
+++ TheMotivator.java
@@ -6,8 +6,11 @@ public class TheMotivator
 			System.out.println("You're awesome");
 		else if (score > 90)
 			System.out.println("That's great");
+		else if (score > 80)
+			System.out.println("That's good");
 		else if (score > 60)
 			System.out.println("That's good ");
+		
 		else
 			System.out.println("Well, what can I say?");
 	}
